6c0bf076af93c320f8a1ce15a3214a38c9a66426
Jonathan Voss <k98kurz@gmail.com>
2024-10-23 11:12:54 -0400

    slightly changed EntryType docstring

--- simplebooks/asyncql/EntryType.py
+++ simplebooks/asyncql/EntryType.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 
 class EntryType(Enum):
-    """Enum of valid Entry types (CREDIT and DEBIT)."""
+    """Enum of valid Entry types: CREDIT and DEBIT."""
     CREDIT = 'c'
     DEBIT = 'd'
