93515f58d7b8f6c9d7096decd24503cfc1390386
Jonathan Voss <k98kurz@gmail.com>
2024-11-11 21:34:36 -0500

    removed unused and incomplete class; added version string to module

--- simplebooks/__init__.py
+++ simplebooks/__init__.py
@@ -12,6 +12,8 @@ from .models import (
 )
 import sqloquent.tools
 
+__version__ = '0.1.0'
+
 def set_connection_info(db_file_path: str):
     """Set the connection info for all models to use the specified
         sqlite3 database file path.
