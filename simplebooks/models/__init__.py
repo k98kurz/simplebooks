e3e61364372332fd8f813c57336502e16ba31c2a
Jonathan Voss <k98kurz@gmail.com>
2025-05-23 21:33:16 -0400

    moved some db management logic to models/__init__.py

--- simplebooks/models/__init__.py
+++ simplebooks/models/__init__.py
@@ -14,6 +14,8 @@ from .Statement import Statement
 from .Transaction import Transaction
 from .Vendor import Vendor
 from sqloquent import contains, within, has_many, belongs_to
+from typing import Callable
+import sqloquent.tools
 
 
 Identity.ledgers = has_many(Identity, Ledger, 'identity_id')
@@ -56,3 +58,65 @@ Account.archived_entries = has_many(Account, ArchivedEntry, 'account_id')
 
 ArchivedTransaction.ledgers = contains(ArchivedTransaction, Ledger, 'ledger_ids')
 Ledger.archived_transactions = within(Ledger, ArchivedTransaction, 'ledger_ids')
+
+
+def set_connection_info(db_file_path: str):
+    """Set the connection info for all models to use the specified
+        sqlite3 database file path.
+    """
+    Account.connection_info = db_file_path
+    AccountCategory.connection_info = db_file_path
+    ArchivedEntry.connection_info = db_file_path
+    ArchivedTransaction.connection_info = db_file_path
+    Currency.connection_info = db_file_path
+    Customer.connection_info = db_file_path
+    Entry.connection_info = db_file_path
+    Identity.connection_info = db_file_path
+    Ledger.connection_info = db_file_path
+    Statement.connection_info = db_file_path
+    Transaction.connection_info = db_file_path
+    Vendor.connection_info = db_file_path
+
+def get_migrations() -> dict[str, str]:
+    """Returns a dict mapping model names to migration file content strs."""
+    models = [
+        Account,
+        AccountCategory,
+        ArchivedEntry,
+        ArchivedTransaction,
+        Currency,
+        Customer,
+        Entry,
+        Identity,
+        Ledger,
+        Statement,
+        Transaction,
+        Vendor,
+    ]
+    migrations = {}
+    for model in models:
+        migrations[model.__name__] = sqloquent.tools.make_migration_from_model(model)
+    return migrations
+
+def publish_migrations(
+        migration_folder_path: str,
+        migration_callback: Callable[[str, str], str] = None
+    ):
+    """Writes migration files for the models. If a migration callback is
+        provided, it will be used to modify the migration file contents.
+        The migration callback will be called with the model name and
+        the migration file contents, and whatever it returns will be
+        used as the migration file contents.
+    """
+    sqloquent.tools.publish_migrations(migration_folder_path)
+    migrations = get_migrations()
+    for name, m in migrations.items():
+        m2 = migration_callback(name, m) if migration_callback else m
+        m = m2 if type(m2) is str and len(m2) > 0 else m
+        with open(f'{migration_folder_path}/create_{name}.py', 'w') as f:
+            f.write(m)
+
+def automigrate(migration_folder_path: str, db_file_path: str):
+    """Executes the sqloquent automigrate tool."""
+    sqloquent.tools.automigrate(migration_folder_path, db_file_path)
+
